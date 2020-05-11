a19f02c7d6db0e1d121c614f4f8a7d927b88a6d5
mariag39 <mariag39@ucm.es>
2020-05-11 02:12:46 +0200

    intentando cosas chachis7

---
+++ button.js
@@ -0,0 +1,36 @@
+/*~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+	Copyright (c) 2012 Brett Wejrowski
+
+	wojodesign.com
+	simplecartjs.org
+	http://github.com/wojodesign/simplecart-js
+
+	VERSION 3.0.5
+
+	Dual licensed under the MIT or GPL licenses.
+~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~*/
+/*jslint browser: true, unparam: true, white: true, nomen: true, regexp: true, maxerr: 50, indent: 4 */
+
+(function (window, document) {
+	//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+	
+}(window, document));
+
